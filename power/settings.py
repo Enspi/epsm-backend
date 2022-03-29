3662449537e3adefd3a648e95bff578f65c1d826
Enspi <94926571+Enspi@users.noreply.github.com>
2022-03-29 15:31:56 +0500

    Update settings.py

--- power/settings.py
+++ power/settings.py
@@ -29,13 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if env('ENV') == "development" else False
 
-# ALLOWED_HOSTS = ["*"]
-# ALLOWED_HOSTS = ["localhost", "127.0.0.1", "power-moses.herokuapp.com", "master.d3uboawzuu8f07.amplifyapp.com"]
-
-
-ALLOWED_HOSTS=['master.d3uboawzuu8f07.amplifyapp.com']
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ('master.d3uboawzuu8f07.amplifyapp.com')
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "power-moses.herokuapp.com", "master.d3uboawzuu8f07.amplifyapp.com"]
 
 
 # Application definition
@@ -155,8 +149,7 @@ JWT_MAX_AGE = 24 * 60 * 60
 
 # CORS
 CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
