52dc009ca8d7a6303df9eb4ac7d7d0cb712d6ca9
Enspi <94926571+Enspi@users.noreply.github.com>
2022-03-29 15:09:34 +0500

    Update settings.py

--- power/settings.py
+++ power/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if env('ENV') == "development" else False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "power-moses.herokuapp.com", "master.d3uboawzuu8f07.amplifyapp.com"]
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["localhost", "127.0.0.1", "power-moses.herokuapp.com", "master.d3uboawzuu8f07.amplifyapp.com"]
 
 
 # Application definition
